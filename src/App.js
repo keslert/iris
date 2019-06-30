fcaebd76f6954dfff6bcd786d2ca83f0b8816679
annellee <alee7368@stanford.edu>
2019-06-29 22:05:27 -0700

    Clicking color on wheel updates hex code

--- src/App.js
+++ src/App.js
@@ -1,145 +1,86 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Flex, Box } from 'rebass';
 import ColorWheel from './components/ColorWheel';
 import ColorGradient from './components/ColorGradient';
 
-// colors for each color circle (from outer to inner)
-const defaultWheelColors1 = [
-  '#F6A72F',
-  '#F17AAF',
-  '#897AB9',
-  '#47B1E4',
-  '#87D4EB',
-  '#91D0B4',
-  '#F5D379',
-  '#0B3243',
-  '#B8C4CC',
-  '#E5E5E5',
-  '#989798',
-  '#4D4D4D'
-]
+/* I followed a tutorial to get the color of a pixel on click. 
+ * Below is the code from the tutorial. */ 
+var canvas = document.getElementById("canvas");
+var hex = "#4e73ad";
 
-const defaultWheelColors2 = [
-  '#F9BD65',
-  '#F89CC5',
-  '#B39CF7',
-  '#65C7F8',
-  '#9CE5F8',
-  '#9CF8D3',
-  '#F9E09D', 
-  '#4A6471', 
-  '#CBD4D9', 
-  '#ECECEC',
-  '#B3B3B3', 
-  '#7A7A7A', 
-]
+function getElementPosition(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
+}
 
-const defaultWheelColors3 = [
-  '#FACE8D',
-  '#FAB5D4',
-  '#C6B5FB',
-  '#A8E1FE',
-  '#B4ECFB',
-  '#B5FBDF',
-  '#FBE7B4',
-  '#788C95',
-  '#D7DFE2',
-  '#F1F1F1',
-  '#C6C6C6',
-  '#9B9B9B',
-]
+function getEventLocation(element,event){
+		var pos = getElementPosition(element);
+    
+    return {
+    		x: (event.pageX - pos.x),
+      	y: (event.pageY - pos.y)
+    };
+}
 
-const defaultWheelColors4 = [
-  '#FCDAAC',
-  '#FCC8DF',
-  '#D5C8FD',
-  '#A9E1F8',
-  '#C8F3FC',
-  '#C7FBE5', 
-  '#FCEEC7',
-  '#99AAB4',
-  '#E3E8EC',
-  '#F5F5F5',
-  '#D4D4D4',
-  '#B4B4B4',
-]
-
-const defaultWheelColors5 = [
-  '#FAE5B8',
-  '#FCD6E5',
-  '#E0D6FB',
-  '#C1E7FC',
-  '#D8F6FE',
-  '#D5FEEC', 
-  '#FCF3D6',
-  '#B4BFC5',
-  '#E9EDEE',
-  '#F8F8F8',
-  '#DFDFDF',
-  '#C7C7C7',
-]
-
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
 
 // colors for gradient bars
-const defaultGradients = [
-  {start: defaultWheelColors1[1], end: defaultWheelColors1[0]},
-  {start: defaultWheelColors1[2], end: defaultWheelColors1[3]},
-  {start: defaultWheelColors1[1], end: defaultWheelColors1[2]},
-  {start: defaultWheelColors1[5], end: defaultWheelColors1[4]},
+var defaultGradients = [
+  {start: hex, end: hex},
+  {start: hex, end: hex},
+  {start: hex, end: hex},
+  {start: hex, end: hex},
 ]
 
+canvas.addEventListener("click",function(e){
+	var eventLocation = getEventLocation(this,e);
+    var coord = "x=" + eventLocation.x + ", y=" + eventLocation.y;
+    
+    // get the data of the pixel according to the location generate by the getEventLocation function
+    var context = this.getContext('2d');
+    var pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data; 
+
+    // if transparency on the image
+    if((pixelData[0] == 0) && (pixelData[1] == 0) && (pixelData[2] == 0) && (pixelData[3] == 0)){
+ 				coord += " (Transparent color detected, cannot be converted to HEX)";
+    }
+    
+	// convert to hex
+    hex = "#" + ("000000" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);
+    
+	// update colors for gradient bars
+	// I thought that if this was updated React would automatically update the colors of the gradients since the state changed? 
+	defaultGradients = [
+		{start: hex, end: hex},
+		{start: hex, end: hex},
+	    {start: hex, end: hex},
+		{start: hex, end: hex},
+	]
+
+    // Draw the color and coordinates.
+    document.getElementById("hex").innerHTML = hex;
+	document.getElementById("hex").style.color = hex; 
+	ReactDOM.render(<App />, document.getElementById('root')); // rerender? not sure if this is right
+},false);
+
 function App() {
-  const [wheelColors, setWheelColors] = React.useState(defaultWheelColors1)
+  // const [wheelColors, setWheelColors] = React.useState(defaultWheelColors1);
   const [gradients, setGradients] = React.useState(defaultGradients);
 
   return (
     <Flex p={3}>
-	  <Box>
-        <ColorWheel 
-          defaultColors={defaultWheelColors1}
-		  radius={500}
-		  color={true}
-		  off={'50px'}
-        />
-      </Box>
-	  <Box>
-        <ColorWheel 
-          defaultColors={defaultWheelColors2}
-		  radius={400}
-		  color={false}
-		  off={'100px'}
-        />
-      </Box>
-
-	  <Box>
-        <ColorWheel 
-          defaultColors={defaultWheelColors3}
-		  radius={300}
-		  color={false}
-		  off={'150px'}
-        />
-      </Box>
-
-	  <Box>
-        <ColorWheel 
-          defaultColors={defaultWheelColors4}
-		  radius={200}
-		  color={false}
-		  off={'200px'}
-        />
-      </Box>
-
-	  <Box>
-        <ColorWheel 
-          defaultColors={defaultWheelColors5}
-		  radius={100}
-		  color={false}
-		  off={'250px'}
-        />
-      </Box>
-
-
-
       <Flex flexDirection="column">
         {gradients.map(gradient => (
           <Box p={2}>
@@ -154,21 +95,6 @@ function App() {
     </Flex>
   );
 }
-
-/*
-	  <div className="angled">
-	  <Flex flexDirection="column">
-        {gradients.map(gradient => (
-          <Box p={2}>
-            <ColorGradient 
-              defaultGradient={gradient}
-			  angle = {75}
-            />
-          </Box>
-        ))}
-      </Flex>
-	  </div>
-*/ 
 
 export default App;
 
